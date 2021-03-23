a819c8521cbda1a5c43f8dbea6f1e0e53656bf08
David Li <37282826+davidli731@users.noreply.github.com>
2021-03-24 08:49:40 +1100

    added react-router-dom to App.tsx

--- travelogue-web/src/App.tsx
+++ travelogue-web/src/App.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
 import Auth from "./Components/BackEndLogic/Auth";
+//import AccountsPage from "./Components/Accounts/accounts_page";
 
 function App() {
   return (
-    <div className="App">
-      <Auth>
-        <p>a</p>
-        <p>b</p>
-      </Auth>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Route exact={true} path="/" render={() => (
+          <div className="App">
+            <Auth>
+              <p>a</p>
+              <p>b</p>
+            </Auth>
+          </div>
+        )} />
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
