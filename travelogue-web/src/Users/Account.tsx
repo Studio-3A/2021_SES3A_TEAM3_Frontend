1870d2700b476c4daf2aa9d91f637788de389567
David Li <37282826+davidli731@users.noreply.github.com>
2021-03-28 15:30:25 +1100

    Create Account.tsx

---
+++ travelogue-web/src/Users/Account.tsx
@@ -0,0 +1,19 @@
+// TODO:
+//   -  Create profile/accounts page for the user
+
+import React from 'react';
+import { BrowserRouter, withRouter } from 'react-router-dom';
+
+// TODO:
+function Account() {
+
+    return(
+        <BrowserRouter>
+            <div className="App">
+            </div>
+        </BrowserRouter>
+    );
+
+}
+
+export default withRouter(Account);
