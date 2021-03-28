e03bd7c0e44591e01e2ad5fa9e1898b1894a9d93
David Li <37282826+davidli731@users.noreply.github.com>
2021-03-28 15:27:26 +1100

    added NavBar

--- travelogue-web/src/App.tsx
+++ travelogue-web/src/App.tsx
@@ -4,13 +4,32 @@ import logo from './logo.svg';
 import './App.css';
 
 import Auth from "./Components/BackEndLogic/Auth";
-//import AccountsPage from "./Components/Accounts/accounts_page";
+import Account from "./Users/Account";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <React.Fragment>
+        <NavBar/>
         <Route exact={true} path="/" render={() => (
+          <div className="App">
+          </div>
+        )} />
+        <Route exact={true} path="/about" render={() => (
+          <div className="App">
+          </div>
+        )} />
+        <Route exact={true} path="/features" render={() => (
+          <div className="App">
+          </div>
+        )} />
+        <Route exact={true} path="/account" render={() => (
+          <div className="App">
+            <Account/>
+          </div>
+        )} />
+        <Route exact={true} path="/login" render={() => (
           <div className="App">
             <Auth>
               <p>a</p>
