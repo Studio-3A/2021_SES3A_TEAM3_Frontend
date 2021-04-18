60ad97760eb78471335ab5686b34114deebf52d5
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2021-04-18 16:14:07 +1000

    Refactored Activity Typings based on backend data output.
    Adjusted Nav Styling
    
    Adjusted Activities styling
    
    Adjusted Card styling
    
    Co-authored-by: GiorgioBaz <GiorgioBaz@users.noreply.github.com>
    Co-authored-by: jsaji <jsaji@users.noreply.github.com>

--- travelogue-web/src/Components/Home/Trips/Activities.tsx
+++ travelogue-web/src/Components/Home/Trips/Activities.tsx
@@ -1,42 +1,29 @@
 import { StringDecoder } from 'node:string_decoder';
 import React from 'react';
+import LocationImage1 from '../../Images/home-card-bg1.svg';
+import LocationImage2 from '../../Images/home-card-bg2.svg';
+import moment from 'moment';
 
 export interface IState{
-    trip: {
-        day: Day[];
-    }
+    trip: Activity[];
 }
-
-interface Day {
-    date: string,
-    activities: Activity[];
-}
-   
 
 interface Activity {
-    name?: string,
-    price?: string,
-    location?: string,
-    description?: string,
-    time?: string,
-    people?: string
+    types?: string[];
+    name?: string;
+    price?: number;
+    location?: string;
+    description?: string;
+    time?: number;
+    rating?:number;
+    people?: string;
+    duration?: number;
 }
 
-interface TripProps {
-    day: Day;
-    index?: number;
-}
-
-
-const TripDay = (props: TripProps) => {
-    return(
-        <div>
-
-            <p>Day - {props.day.date}</p>
-            {props.day.activities.map(activity => {
-                return(<Activity activity={activity}/>)
-            })
-            }
+const TripDay = (props: IState) => {
+    return (
+        <div className="tripday-block">
+            {props.trip.map(a => <Activity activity={a}/>)}
         </div>
     );
 }
@@ -45,38 +32,62 @@ interface ActProps {
     activity: Activity;
 }
 
-const Activity = (props: ActProps) => {
-    return (
-        
-        <div className="activity-block">
-            <div className="activity-card card card-shadow">
-
-            </div>
-            <div className="activity-description">
-                <h3>Description</h3>
-                
-                <div className= "activity-details"> 
-                <p>{props.activity.description}</p>
-
-                </div>
-            </div>
-        </div>
-    );
+interface BitProps {
+    label: string;
+    value: string | number | undefined;
+    className?: string;
 }
 
+const ActivityProperty = (props: BitProps) => {
+    const { value } = props;
+    const className = props.className || "";
+    return (
+        <>
+            {value && 
+                <div className={`activity-label ${className}`}>{value}</div>
+            }
+        </>
+    );
+};
 
-export const Activities = (state: IState) => {
-    return(
-        <div>
-            <p className="page-label">Activities</p>
-            {state.trip.day.map(d  => {
-                return(
-                    <div>
-                        <TripDay day={d}/>
-                    </div>
-                )
-            })}
+const Activity = (props: ActProps) => {
+    const { description, name, price, location, time, duration } = props.activity;
+    const formattedPrice = price != null ? `$${price}` : price;
+    const timeFormat = 'h:m A';
+    const actualTime = time != null && duration != null ? (moment(time).format(timeFormat) + ' - ' + moment(time).add({minute: duration}).format(timeFormat)) : time;
+
+    /* moment().add(Number, String);
+    moment().add(Duration);
+    moment().add(Object);*/
+    
+    return (
+        <div className="activity-block">
+            <div className="activity-card card card-shadow">
+                <div className="activity-img-backdrop">
+                <ActivityProperty label={"Name"} value={name} className={"h3"}  />
+                <ActivityProperty label={"Description"} value={description} />
+                <ActivityProperty label={"Price"} value={formattedPrice} className={"activity-price"}  />
+                <ActivityProperty label={"Location"} value={location} className={"activity-location"} />
+                </div>
+            </div>
+            <div className="activity-description">
+                <ActivityProperty label={"Time"} value={actualTime} />
+                <ActivityProperty label={"Duration"} value={duration} />
+                <ActivityProperty label={"Description"} value={description} />
+            </div>
             
         </div>
     );
-}
+};
+
+
+export const Activities = (state: IState) => {
+    return (
+        <div className="activities-block">
+            <p className="page-label">Activities</p>
+            <div className="activities-tripday-block">
+                <TripDay trip={state.trip}/>
+            </div>
+        </div>
+    );
+};
