7c18466cca1b69f91409c8253fec436250f72ff2
omer.samoel <12962395@student.uts.edu.au>
2021-05-17 14:19:00 +1000

    Created test trip json

--- travelogue-web/src/Components/SavedTrips/SavedTrips.tsx
+++ travelogue-web/src/Components/SavedTrips/SavedTrips.tsx
@@ -6,24 +6,41 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Trips from './Trips';
 import './SavedTrips.css';
-
+import SavedTrip  from "../../Components/savedtrips.json";
 // Images
 import SavedTripsIcon from "../../Images/saved-trips-heart1.svg";
-
-interface State {
+import firebase from '../../firebase/firebase';
+import { isNullOrUndefined } from 'node:util';
+interface IState {
+    userId: string;
 }
 
-// TODO:
-class SavedTrips extends React.Component<{}, State> {
-
+class SavedTrips extends React.Component<{}, IState> {
+    
     constructor(props: {}) {
         super(props);
 
         this.state = {
+            userId: ''
+
         };
     }
-
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+			if (user?.uid) {
+				this.setState({ userId: String(user?.uid)});
+			} 
+            else{
+                this.setState({ userId:'1'});
+            }
+		});
+    }
     render() {
+        let testing = SavedTrip.users.find(user => user.id === this.state.userId);
+        let savedTrips = testing?.savedtrips.map(trip => {
+            return (<div> <Trips  key={trip.trip_id} /></div>)
+        });
+        
         return (
             <BrowserRouter>
                 <div className="SavedTrips">
@@ -39,7 +56,7 @@ class SavedTrips extends React.Component<{}, State> {
                     </div>
 
                     {/*Main body*/}
-                    <Trips/>
+                    <div >{savedTrips}</div>
                 </div>
             </BrowserRouter >
         );
