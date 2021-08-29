eacbb4fff68324cae29f33def27a6c5f3089c690
Emanuel <emanuel.kindaro@student.uts.edu.au>
2021-08-29 22:45:39 +1000

    Added Co-Author
    Co-authored-by: wasifhossainn <wasif.hossain@hotmail.com>

--- travelogue-web/src/index.tsx
+++ travelogue-web/src/index.tsx
@@ -8,7 +8,7 @@ const express = require('express');
 const app = express();
  
 app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
+  console.log('Listening on port 3000!'),
 );
 
 /** 
