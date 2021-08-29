d5b2d139e4295ff87b64de336fa7f272dfc4459d
Emanuel <emanuel.kindaro@student.uts.edu.au>
2021-08-29 22:26:02 +1000

    express setup

--- travelogue-web/src/index.tsx
+++ travelogue-web/src/index.tsx
@@ -3,7 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const express = require('express');
 
+const app = express();
+ 
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
+
+/** 
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +23,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
