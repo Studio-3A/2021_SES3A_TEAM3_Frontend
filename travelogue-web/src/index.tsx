b43d89bec4b0ee33c084db49665b8d9d05727dcd
wasifhossainn <wasif.hossain@hotmail.com>
2021-09-05 20:00:41 +1000

    Added apiRoutes and authRoutes and provided internal documentation
    Co-Authored-By: Emanuel <40686034+UnitedEK@users.noreply.github.com>

--- travelogue-web/src/index.tsx
+++ travelogue-web/src/index.tsx
@@ -6,12 +6,12 @@ import reportWebVitals from './reportWebVitals';
 const express = require('express');
 
 const app = express();
- 
+
 app.listen(3000, () =>
   console.log('Listening on port 3000!'),
 );
 
-/** 
+/**
 ReactDOM.render(
   <React.StrictMode>
     <App />
