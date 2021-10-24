e69f5b5bad167c910295c6aca8b190b62d6ae7d0
HdroguettA <48663306+HdroguettA@users.noreply.github.com>
2021-10-24 18:57:27 +1100

    Stlying and icon for the chatbot open and close functionality for the chat

--- travelogue-web/src/Components/Common/ChatBot/ChatBot.tsx
+++ travelogue-web/src/Components/Common/ChatBot/ChatBot.tsx
@@ -1,18 +1,43 @@
 import './ChatBot.css';
+import '../../../App.css';
+import star2 from '../../svg/chatbot.svg';
 
 function ChatBot() {
 
+    function handleChatButton() {
+        const el: HTMLElement = document.getElementById("chatbot-window")!;
+        let currentDisplay = el.style.display;
+
+        if (currentDisplay === 'block') {
+            el.style.display = 'none';
+        } else {
+            console.log('here');
+            el.style.display = 'block';
+        }
+    }
     
     return (
         <div className="cb-container">
-            <div className="cb-button">
-            <iframe
-                allow='microphone;'
-                width='350'
-                height='430'
-                src='https://console.dialogflow.com/api-client/demo/embedded/44cd97b1-9772-4c73-a472-204fbe864c8c'
-            ></iframe>
+            <div className="cb-section">
+                <div id="chatbot-window" className="cb-chatbot">
+                    <div className="cb-chatbot-container">
+                        <iframe className="chatbot-iframe card-shadow"
+                            allow='microphone;'
+                            width='350'
+                            height='430'
+                            src='https://console.dialogflow.com/api-client/demo/embedded/44cd97b1-9772-4c73-a472-204fbe864c8c'>
+                        </iframe>
+    </div> 
+                </div>
+                <div className="cb-button">
+                    
+                    <div id="chatbot-btn" className="cb-button-ico" onClick={() => handleChatButton()}>
+                    <img src={star2} />
+  
+                    </div>
+                </div>
             </div>
+
         </div>
     );
 }
